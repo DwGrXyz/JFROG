f8a9896819bcb95a4151180a146bd0cb495aa11c
DwGr <dwarfgrakula@gmail.comm>
2025-03-17 21:28:11 +0400

    Add ability to create item in ProjectList

--- src/domains/project/ProjectList.vue
+++ src/domains/project/ProjectList.vue
@@ -9,7 +9,7 @@
         link
         :to="getProjectDetailsRoute(project.id)"
       >
-        <v-list-item-title> {{ project.title }}</v-list-item-title>
+        <v-list-item-title>{{ project.title }}</v-list-item-title>
 
         <template v-slot:append>
           <v-btn
@@ -20,6 +20,15 @@
           />
         </template>
       </v-list-item>
+
+      <v-list-item link @click="createProject">
+        <v-list-item-title>
+          <div class="d-flex align-center ga-1">
+            <v-icon :icon="mdiPlus" size="small" />
+            Create
+          </div>
+        </v-list-item-title>
+      </v-list-item>
     </v-list>
   </div>
 </template>
@@ -27,12 +36,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { projectListDefault } from './ProjectModel'
-import { getProjectDetailsRoute } from '@/router'
-import { mdiClose } from '@mdi/js'
+import { getProjectDetailsRoute, getProjectFormRoute } from '@/router'
+import { mdiClose, mdiPlus } from '@mdi/js'
+import { useRouter } from 'vue-router'
 
 const projectList = ref(projectListDefault)
 
 const removeProject = (id: string) => {
   projectList.value = projectList.value.filter((project) => project.id !== id)
+}
+
+const router = useRouter()
+const createProject = () => {
+  const id = '3'
+  const route = getProjectFormRoute(id)
+  router.push(route)
 }
 </script>
