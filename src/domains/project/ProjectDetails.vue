d89b1e3a7acfd4b7b6a7818e1b2de62b5753c083
DwGr <dwarfgrakula@gmail.comm>
2025-03-17 23:36:56 +0400

    Add edit button to ProjectDetails and clone markup to ProjectFrom

--- src/domains/project/ProjectDetails.vue
+++ src/domains/project/ProjectDetails.vue
@@ -1,6 +1,8 @@
 <template>
   <NotFoundPage v-if="!project" />
-  <AppLayout v-else :title="project.title" />
+  <AppLayout v-else :title="project.title">
+    <v-btn :to="formRoute" text="Edit" color="primary" />
+  </AppLayout>
 </template>
 
 <script setup lang="ts">
@@ -8,6 +10,8 @@ import { useAppStore } from '@/store'
 import { computed } from 'vue'
 import type { ProjectModel } from './store/projectModel'
 import NotFoundPage from '@/components/NotFoundPage.vue'
+import { getProjectFormRoute } from '@/router'
+import AppLayout from '@/components/AppLayout.vue'
 
 const props = defineProps<{
   projectId: string
@@ -17,4 +21,6 @@ const store = useAppStore()
 const project = computed<ProjectModel | undefined>(() =>
   store.getters['projects/getProject'](props.projectId),
 )
+
+const formRoute = computed(() => getProjectFormRoute(props.projectId))
 </script>
--- src/domains/project/ProjectForm.vue
+++ src/domains/project/ProjectForm.vue
@@ -1,7 +1,21 @@
-<template>Project form: {{ projectId }}</template>
+<template>
+  <NotFoundPage v-if="!project" />
+  <AppLayout v-else :title="project.title" />
+</template>
 
 <script setup lang="ts">
-defineProps<{
+import { useAppStore } from '@/store'
+import { computed } from 'vue'
+import type { ProjectModel } from './store/projectModel'
+import NotFoundPage from '@/components/NotFoundPage.vue'
+import AppLayout from '@/components/AppLayout.vue'
+
+const props = defineProps<{
   projectId: string
 }>()
+
+const store = useAppStore()
+const project = computed<ProjectModel | undefined>(() =>
+  store.getters['projects/getProject'](props.projectId),
+)
 </script>
