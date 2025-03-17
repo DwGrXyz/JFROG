e080032e0f5629b14ec3407fc50c1a444c44da54
DwGr <dwarfgrakula@gmail.comm>
2025-03-17 20:14:47 +0400

    Add list to ProjectList

--- src/domains/project/ProjectList.vue
+++ src/domains/project/ProjectList.vue
@@ -1,1 +1,27 @@
-<template>Project list</template>
+<template>
+  <div>
+    <h1>Project list</h1>
+
+    <v-list>
+      <v-list-item
+        v-for="project in projectList"
+        :key="project.id"
+        link
+      >
+        <RouterLink
+          :to="getProjectDetailsRoute(project.id)"
+        >
+          {{ project.title }}
+        </RouterLink>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { projectListDefault } from './ProjectModel'
+import { getProjectDetailsRoute } from '@/router'
+
+const projectList = ref(projectListDefault)
+</script>
---
+++ src/domains/project/ProjectModel.ts
@@ -0,0 +1,9 @@
+export type ProjectModel = {
+  id: string
+  title: string
+}
+
+export const projectListDefault: ProjectModel[] = [
+  { id: '1', title: 'Project 1' },
+  { id: '2', title: 'Project 2' },
+]
