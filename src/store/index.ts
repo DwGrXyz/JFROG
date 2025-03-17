5356844398bc77eb6f7bfe4b36a91d56d3ee21fc
DwGr <dwarfgrakula@gmail.comm>
2025-03-17 22:56:03 +0400

    Fix types in vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,16 @@
 import projectStoreModule, {
   type ProjectStoreState,
 } from '@/domains/project/store'
-import type { InjectionKey } from 'vue'
-import { createStore, Store, useStore } from 'vuex'
+import { createStore, useStore } from 'vuex'
 
 export interface State {
   projects: ProjectStoreState
 }
 
-export const key: InjectionKey<Store<State>> = Symbol()
-
-export default createStore({
+export default createStore<State>({
   modules: {
     projects: projectStoreModule,
   },
 })
 
-export const useAppStore = () => useStore(key)
+export const useAppStore = () => useStore()
