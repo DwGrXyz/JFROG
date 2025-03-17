6ccaadde0f583eaadc409742a7c2bed08aad5fae
DwGr <dwarfgrakula@gmail.comm>
2025-03-17 23:21:47 +0400

    Show title in ProjectDetails

--- src/domains/project/ProjectDetails.vue
+++ src/domains/project/ProjectDetails.vue
@@ -1,7 +1,22 @@
-<template>Project details: {{ projectId }}</template>
+<template>
+  <template v-if="project">
+    <h1>{{ project?.title }}</h1>
+  </template>
+  <NotFoundPage v-else />
+</template>
 
 <script setup lang="ts">
-defineProps<{
+import { useAppStore } from '@/store'
+import { computed } from 'vue'
+import type { ProjectModel } from './store/projectModel'
+import NotFoundPage from '@/NotFoundPage.vue'
+
+const props = defineProps<{
   projectId: string
 }>()
+
+const store = useAppStore()
+const project = computed<ProjectModel | undefined>(() =>
+  store.getters['projects/getProject'](props.projectId),
+)
 </script>
