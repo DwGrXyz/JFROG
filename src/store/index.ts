6aaea4e9b9fd3e2e3b72c83abe31e8ba35c7924b
DwGr <dwarfgrakula@gmail.comm>
2025-03-18 10:58:31 +0400

    Add vuex-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,21 @@ import projectStoreModule, {
   type ProjectStoreState,
 } from '@/domains/project/store'
 import { createStore, useStore } from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 export interface State {
   projects: ProjectStoreState
 }
 
+const vuexLocal = new VuexPersistence<State>({
+  storage: window.localStorage,
+})
+
 export default createStore<State>({
   modules: {
     projects: projectStoreModule,
   },
+  plugins: [vuexLocal.plugin],
 })
 
 export const useAppStore = () => useStore()
