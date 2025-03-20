2483bbbefb9b8730657fd88f7045fdef52193802
DwGr <dwarfgrakula@gmail.comm>
2025-03-20 11:56:23 +0400

    Check task in projectDetailsRoute and projectEditRoute

--- cypress/e2e/projectDetailsRoute.cy.ts
+++ cypress/e2e/projectDetailsRoute.cy.ts
@@ -1,3 +1,5 @@
+import type { TaskForm } from '../../src/domains/task/store/taskModel'
+
 describe('ProjectDetailsRoute', () => {
   it('Default content', () => {
     cy.viewNewProject('Project1')
@@ -9,5 +11,19 @@ describe('ProjectDetailsRoute', () => {
     cy.editNewProject('Project1')
     cy.contains('h1', 'Edit project')
     cy.location('pathname').should('contain', '/edit')
+  })
+
+  it('Task', () => {
+    const task: TaskForm = {
+      title: 'Task1-title',
+      description: 'Task1-description',
+      priority: 'high',
+      status: 'completed',
+      dueDate: '2011-11-11',
+    }
+    cy.createNewTask('Project1', task)
+    cy.get('[data-cy="cancel"]').click()
+    cy.checkTasksCount(1)
+    cy.checkTaskFields(task)
   })
 })
--- cypress/e2e/projectEditRoute.cy.ts
+++ cypress/e2e/projectEditRoute.cy.ts
@@ -1,3 +1,5 @@
+import type { TaskForm } from '../../src/domains/task/store/taskModel'
+
 describe('ProjectEditRoute', () => {
   it('Default content', () => {
     cy.editNewProject('Project1')
@@ -30,5 +32,18 @@ describe('ProjectEditRoute', () => {
     cy.viewNewTask('Project1')
     cy.contains('h1', 'Create task')
     cy.location('pathname').should('contain', '/tasks/new')
+  })
+
+  it('Task', () => {
+    const task: TaskForm = {
+      title: 'Task1-title',
+      description: 'Task1-description',
+      priority: 'high',
+      status: 'completed',
+      dueDate: '2011-11-11',
+    }
+    cy.createNewTask('Project1', task)
+    cy.checkTasksCount(1)
+    cy.checkTaskFields(task)
   })
 })
